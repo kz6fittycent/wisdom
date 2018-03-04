2afe14a93b8a77ad7d4b1463e1e359c709a54d62
kz6fittycent <jimmy.tigert@gmail.com>
2018-03-03 21:21:13 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="wisdom",
+    version="1.0.0",
+    description="A collection of wise quotes for the terminal",
+    long_description=open("README.md").read(),
+    license="MIT",
+    packages=["libwisdom"],
+    scripts=["wisdom"],
+    package_data={"libwisdom": ["data/*"]},
+)
